1cca883d790e3945b88492a7b033b45400df9cb3
[clara rabasseda cases] <[crabasseda@uoc.edu]>
2025-03-23 20:00:36 +0100

    web responsive

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,16 +1,4 @@
-/**
- * Import dependencies from node_modules
- * see commented examples below
- */
-
-// import 'some-node-module';
-// import SomeModule from 'some-node-module';
-
-/**
- * Write any other JavaScript below
- */
-
-+( function() {
-  const university = "UOC";
-  console.log(`Hello, ${university}!`);
-} )();
+document.getElementById("show-bafarada").addEventListener("click", function () {
+  document.querySelector(".bafarada-container").classList.add("visible");
+  this.style.display = "none";
+});
