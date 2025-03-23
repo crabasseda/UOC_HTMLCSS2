8a27d6ab4bff5509dde852e415692795d9b31379
[clara rabasseda cases] <[crabasseda@uoc.edu]>
2025-03-23 20:43:32 +0100

    fix secondary cards

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -2,3 +2,17 @@ document.getElementById("show-bafarada").addEventListener("click", function () {
   document.querySelector(".bafarada-container").classList.add("visible");
   this.style.display = "none";
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  const cards = document.querySelectorAll(".secondary-card");
+
+  cards.forEach((card) => {
+    card.addEventListener("click", () => {
+      const hiddenInfo = card.querySelector(".secondary-card-hidden-info");
+
+      if (window.matchMedia("(hover: none)").matches) {
+        hiddenInfo.classList.toggle("visible");
+      }
+    });
+  });
+});
